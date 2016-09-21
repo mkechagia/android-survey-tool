ab431cba871d75d41c9072fcaff232ce54aeec83
mkechagia <mkechagia@aueb.gr>
2016-09-21 16:10:25 +0300

    Add examples for new checked exceptions

---
+++ InsufficientMemoryForResourceException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app;
+
+/**
+ * This exception is thrown when a resource (i.e. image) stored in drawable
+ * has big size and it cannot be inflated correctly. 
+ */
+public class InsufficientMemoryForResourceException extends Exception {
+
+    public InsufficientMemoryForResourceException() {
+        super();
+    }
+
+    public InsufficientMemoryForResourceException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+
+    public InsufficientMemoryForResourceException(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public InsufficientMemoryForResourceException(Throwable throwable) {
+        super(throwable);
+    }
+
+}
