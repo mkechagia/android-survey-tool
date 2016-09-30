c11303b522942f1a8b5efe1637a31b8c1130d965
mkechagia <mkechagia@aueb.gr>
2016-09-30 11:44:57 +0300

    Fix issue from survey to new user

--- routes.py
+++ routes.py
@@ -90,7 +90,12 @@ def new():
 				else:
 					flash('The email is not valid.', 'error')
 	if ('email' in session):
-		return render_template(set_survey_type(get_rowid(session['email'])), answ = answ)
+		# check if there is already any submission
+		submission = submissions.query.filter_by(email = session['email']).first()
+		if (submission.count_submission != 0):
+			return render_template('new.html')
+		else:
+			return render_template(set_survey_type(get_rowid(session['email'])), answ = answ)
 	else:
 		return render_template('new.html')
 
